d13124c9905608a162cc22352145628087e2154a
GabrielKim2 <113950931+GabrielKim2@users.noreply.github.com>
2023-02-09 20:53:23 -0500

    Started working on getting april tag position

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -15,9 +17,18 @@ public class LimelightSubsystem extends SubsystemBase {
     double x;
     double y;
     double area;
+    double xInRadians;
+    double yInRadians;
 
-    double processedX;
-    double processedY;
+    double distanceX;
+    double distanceY;
+
+    double limelightMountAngle;
+    double limelightHeight;
+    double targetHeight;
+
+    double xToRadians;
+    double yToRadians;
 
     public LimelightSubsystem() {
     }
@@ -27,5 +38,25 @@ public class LimelightSubsystem extends SubsystemBase {
         x = tx.getDouble(0.0);
         y = ty.getDouble(0.0);
         area = ta.getDouble(0.0);
+        // SmartDashboard.putNumber("Offset X", x);
+        // SmartDashboard.putNumber("Offset Y", y);
+        // SmartDashboard.putNumber("Targe Area", area);
+        // Convert X and Y to radians to use with Math.tan
+        xInRadians = x * (3.14159 / 180);
+        yInRadians = y * (3.14159 / 180);
+        // Use offset angle to calculate distances to target
+        distanceX = (limelightHeight - targetHeight);
+    }
+
+    private final GenericEntry LimelightX = Shuffleboard.getTab("VisionTesting").add("Target X Offset", 0)
+            .withPosition(1, 1)
+            .withSize(1, 1).getEntry();
+    private final GenericEntry LimelightY = Shuffleboard.getTab("VisionTesting").add("Target Y Offset", 0)
+            .withPosition(2, 1)
+            .withSize(1, 1).getEntry();
+
+    public void updateWidgets() {
+        LimelightX.setDouble(x);
+        LimelightY.setDouble(y);
     }
 }
