895eaa0ca85e0ee1decdd11790e460cd112982c7
LucasCodee <lucascornejo222@gmail.com>
2023-03-03 18:26:39 -0500

    ADDED ENTIRE LED SUBSYSTEM.

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -13,79 +13,97 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.OperatorConstants.*;
 
 public class LEDSubsystem extends SubsystemBase {
-  /** Creates chargeIndex new LEDSubsytem. */
-  private static final int kLEDPort = 4;
-  private static final int kLEDLength = 66;
+  /** Creates a new LEDSubsytem. */
+  private static final int kLEDPort1 = 4;
+  private static final int kLEDLength1 = 66;
+  private static final int kLEDPort2 = 5;
+  private static final int kLEDLength2 = 23;
   private String currentColor = "yellow";
   private boolean isYellow = false;
+  private boolean TimeToCharge = false;
 
-  AddressableLED led = new AddressableLED(kLEDPort);
+  AddressableLED led1 = new AddressableLED(kLEDPort1);
+  AddressableLED led2 = new AddressableLED(kLEDPort2);
+  AddressableLED[] LEDList = {led1, led2};
+  
 
-  AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(kLEDLength);
+  AddressableLEDBuffer ledBuffer1 = new AddressableLEDBuffer(kLEDLength1);
+  AddressableLEDBuffer ledBuffer2 = new AddressableLEDBuffer(kLEDLength2);
+  AddressableLEDBuffer[] LEDBufferList = {ledBuffer1, ledBuffer2};
 
   public LEDSubsystem() {
-    led.setLength(kLEDLength);
-    led.setData(ledBuffer);
+    led1.setLength(kLEDLength1);
+    led1.setData(ledBuffer1);
+    led2.setLength(kLEDLength2);
+    led2.setData(ledBuffer2);
     allOff();
-    led.start();
+    led1.start();
+    led2.start();
   }
 
-  public void setOneGreen(int index) {
-    ledBuffer.setRGB(index, 0, 255, 0);
+  public void setOneGreen(int index, int LED) {
+    LEDBufferList[LED].setRGB(index, 0, 255, 0);
   }
 
   public CommandBase setAllPurple() {
     return runOnce(
         () -> {
-          for (int i = 0; i < ledBuffer.getLength(); i++) {
-            ledBuffer.setRGB(i, 119, 0, 200);
+          for (int i = 0; i < ledBuffer1.getLength(); i++) {
+            ledBuffer1.setRGB(i, 119, 0, 200);
           }
         });
   }
 
-  public void allPurple() {
-    for (int i = 0; i < ledBuffer.getLength(); i++) {
-      ledBuffer.setRGB(i, 119, 0, 200);
+  public void allPurple(int LED) {
+    for (int i = 0; i < LEDBufferList[LED].getLength(); i++) {
+      LEDBufferList[LED].setRGB(i, 119, 0, 200);
     }
   }
 
-  public void allWhite() {
-    for (int i = 0; i < ledBuffer.getLength(); i++) {
-      ledBuffer.setRGB(i, 255, 255, 255);
+  public void allWhite(int LED) {
+    for (int i = 0; i < LEDBufferList[LED].getLength(); i++) {
+      LEDBufferList[LED].setRGB(i, 255, 255, 255);
     }
   }
 
   public void allOff() {
-    for (int i = 0; i < ledBuffer.getLength(); i++) {
-      ledBuffer.setRGB(i, 0, 0, 0);
+    for(int k = 0; k < LEDBufferList.length; k++)
+      for (int i = 0; i < LEDBufferList[k].getLength(); i++) {
+        LEDBufferList[k].setRGB(i, 0, 0, 0);
+      }
+  }
+
+  public void allOff(int LED) {
+    for (int i = 0; i < LEDBufferList[LED].getLength(); i++) {
+      LEDBufferList[LED].setRGB(i, 0, 0, 0);
     }
   }
 
-  public void allRed() {
-    for (int i = 0; i < ledBuffer.getLength(); i++) {
-      ledBuffer.setRGB(i, 255, 0, 0);
+  public void allRed(int LED) {
+    for (int i = 0; i < LEDBufferList[LED].getLength(); i++) {
+      LEDBufferList[LED].setRGB(i, 255, 0, 0);
     }
   }
 
   public void redStrobe() {
     if(((int)(Timer.getMatchTime()*10)%2) == 1)
-      allRed();
+      allRed(0);
     else
-      allOff();
+      allOff(0);
   }
 
   public CommandBase setAllYellow() {
     return runOnce(
         () -> {
-          for (int i = 0; i < ledBuffer.getLength(); i++) {
-            ledBuffer.setRGB(i, 255, 255, 0);
+          for (int i = 0; i < ledBuffer1.getLength(); i++) {
+            ledBuffer1.setRGB(i, 255, 255, 0);
           }
         });
   }
 
-  public void allYellow() {
-    for (int i = 0; i < ledBuffer.getLength(); i++) {
-      ledBuffer.setRGB(i, 255, 255, 0);
+  public void allYellow(int LED) {
+    for (int i = 0; i < LEDBufferList[LED].getLength(); i++) {
+      LEDBufferList[LED].setRGB(i, 255, 255, 0);
     }
   }
 
@@ -93,11 +111,11 @@ public class LEDSubsystem extends SubsystemBase {
     return runOnce(
         () -> {
           if (isYellow == true) {
-            allPurple();
+            allPurple(0);
             System.out.println("Setting all purple");
             isYellow = false;
           } else {
-            allYellow();
+            allYellow(0);
             System.out.println("Setting all yellow");
             isYellow = true;
           }
@@ -107,9 +125,9 @@ public class LEDSubsystem extends SubsystemBase {
   private int rainbowFirstPixelHue = 1;
 
   public void rainbow() {
-    for (var i = 0; i < ledBuffer.getLength(); i++) {
-      final var hue = (rainbowFirstPixelHue + (i * 180 / ledBuffer.getLength())) % 180;
-      ledBuffer.setHSV(i, hue, 255, 128);
+    for (var i = 0; i < ledBuffer1.getLength(); i++) {
+      final var hue = (rainbowFirstPixelHue + (i * 180 / ledBuffer1.getLength())) % 180;
+      ledBuffer1.setHSV(i, hue, 255, 128);
     }
     rainbowFirstPixelHue += 3;
     rainbowFirstPixelHue %= 180;
@@ -118,9 +136,9 @@ public class LEDSubsystem extends SubsystemBase {
   public CommandBase commandRainbow() {
     return runOnce(
         () -> {
-          for (var i = 0; i < ledBuffer.getLength(); i++) {
-            final var hue = (rainbowFirstPixelHue + (i * 180 / ledBuffer.getLength())) % 180;
-            ledBuffer.setHSV(i, hue, 255, 128);
+          for (var i = 0; i < ledBuffer1.getLength(); i++) {
+            final var hue = (rainbowFirstPixelHue + (i * 180 / ledBuffer1.getLength())) % 180;
+            ledBuffer1.setHSV(i, hue, 255, 128);
 
           }
           rainbowFirstPixelHue += 3;
@@ -135,78 +153,89 @@ public class LEDSubsystem extends SubsystemBase {
             Timer.delay(0.02);
             rainbow();
             System.out.println("Changing rainbow color");
-            led.setData(ledBuffer);
+            led1.setData(ledBuffer1);
           }
         });
   }
 
   /**
-   * inputs angle from gyro and makes the LEDs do pretty colors using chargeIndex 3phase sin wave
-   * @param chargeIndex
+   * inputs angle from gyro and makes the LEDs do pretty colors using a 3phase sin wave
+   * @param a
    */
-  public void setColorWithAngle(double angle) {
-    int chargeIndex = (int)angle;
-    if(chargeIndex > 360 || chargeIndex < -360)  chargeIndex = chargeIndex%360;
+  public void setColorWithAngle(double angle, int LED) {
+    int a = (int)angle;
+    if(a > 360 || a < -360)  a = a%360;
     //logic converting degrees to radians and multiplying by 255
-    int r = Math.abs((int)(255*Math.pow(Math.cos(2*((chargeIndex*Pi)/180)), 3)));
-    int g = Math.abs((int)(255*Math.pow(Math.cos(2*(Pi/3) + ((chargeIndex*Pi)/180)), 3)));
-    int hasCharged = Math.abs((int)(255*Math.pow(Math.cos(2*(((2*Pi)/3) + ((chargeIndex*Pi)/180))), 3)));
+    int r = Math.abs((int)(255*Math.pow(Math.cos(2*((a*Pi)/180)), 3)));
+    int g = Math.abs((int)(255*Math.pow(Math.cos(2*(Pi/3) + ((a*Pi)/180)), 3)));
+    int b = Math.abs((int)(255*Math.pow(Math.cos(2*(((2*Pi)/3) + ((a*Pi)/180))), 3)));
     //setting RGB values
-    for (int i = 0; i < ledBuffer.getLength(); i++) {
-      ledBuffer.setRGB(i, r, g, hasCharged);
+    for (int i = 0; i < ledBuffer1.getLength(); i++) {
+      ledBuffer1.setRGB(i, r, g, b);
     }
   }
 
-  public void setOneYellow(int index) {
-    ledBuffer.setRGB(index, 241, 245, 7);
+  public void setOneYellow(int index, int LED) {
+    LEDBufferList[LED].setRGB(index, 241, 245, 7);
   }
 
-  public void setOneBlue(int index) {
-      ledBuffer.setRGB(index, 0, 0, 255);
+  public void setOneBlue(int index, int LED) {
+      LEDBufferList[LED].setRGB(index, 0, 0, 255);
   }
 
   double BYCycle = 0;
-  public void BlueYellow()
+  public void BlueYellow(int LED)
     {
       BYCycle += 0.1;
-        int chargeIndex = Math.abs((int)(5*Math.cos(BYCycle)))+1;
-        for(int i = 0; i < ledBuffer.getLength(); i++)
+        int a = Math.abs((int)(5*Math.cos(BYCycle)))+1;
+        for(int i = 0; i < LEDBufferList[LED].getLength(); i++)
         {
-            if(i%chargeIndex == 0) {
-                setOneBlue(i);
+            if(i%a == 0) {
+                setOneBlue(i, LED);
             } else {
-                setOneYellow(i);
+                setOneYellow(i, LED);
             }
         }
-        System.out.println(BYCycle + " " + chargeIndex);
+        System.out.println(BYCycle + " " + a);
     }
 
-    public int getLEDBuffer() {
-      return ledBuffer.getLength();
+    public int getLEDBuffer(int LED) {
+      return LEDBufferList[LED].getLength();
     }
 
-    int chargeIndex = 0;
-    boolean hasCharged = false;
-
+    int ChargeIndex = 0;
+    boolean b = false;
     public void chargeReady() {
-      chargeIndex = 0;
-      hasCharged = false;
+      ChargeIndex = 0;
+      b = false;
     }
 
-    public void chargeUP() {
-      if(chargeIndex < ledBuffer.getLength()/2 && hasCharged == false) {
-        setOneBlue(chargeIndex);
-        setOneBlue(ledBuffer.getLength()-1 - chargeIndex);
-      } else {
-        hasCharged = true;
-        chargeIndex = 0;
-      }
-      chargeIndex++;
-      System.out.println(chargeIndex + " " + hasCharged);
-    }
+    // public void chargeUP() {
+    //   if(a < ledBuffer1.getLength()/2 && b == false) {
+    //     setOneBlue(a);
+    //     setOneBlue(ledBuffer1.getLength()-1 - a);
+    //   } else {
+    //     b = true;
+    //     a = 0;
+    //   }
+    //   a++;
+    //   System.out.println(a + " " + b);
+    // }
+    
+  public void chargeUP() {
+    TimeToCharge = true;
+  }
 
+  private int PeriodicRuns = 0;
   public void periodic() {
     // This method will be called once per scheduler run
-    led.setData(ledBuffer);
+    for(int i = 0; i < LEDList.length; i++)
+      LEDList[i].setData(LEDBufferList[i]);
+
+    if(TimeToCharge && PeriodicRuns < LEDBufferList[0].getLength()) {
+      setOneYellow(PeriodicRuns, 0);
+      PeriodicRuns++;
+    }
+    
   }
 }
