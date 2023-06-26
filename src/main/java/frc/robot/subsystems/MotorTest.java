203e4f201e60cf53f576de11b90105582e963cd6
RyanCofer <rcofer56@gmail.com>
2023-06-26 19:15:58 -0400

    Semi-functional PID
    The PID tuning now seems to work and can be changed by the values in shuffleboard.

--- src/main/java/frc/robot/subsystems/MotorTest.java
+++ src/main/java/frc/robot/subsystems/MotorTest.java
@@ -5,10 +5,12 @@ import frc.robot.Constants;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Servo;
 
 import edu.wpi.first.networktables.GenericEntry;
@@ -18,15 +20,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class MotorTest extends SubsystemBase{
-    /*Motor1=CanSparkMax
-    *Motor2=Spark
-    *Motor3=TalonSRX
+    /*Motor1 = CanSparkMax
+    * Motor2 = Spark
+    * Motor3 = TalonSRX
+    * Motor4 = Servo
     */
     private WPI_TalonSRX motor3 = new WPI_TalonSRX(Constants.OperatorConstants.CANTalonSRX);
     private CANSparkMax motor1 = new CANSparkMax(1, MotorType.kBrushless);
     private Spark motor2 = new Spark(Constants.OperatorConstants.kOGSparkPort);
     private Servo motor4 = new Servo(Constants.OperatorConstants.Servo);
     private SparkMaxPIDController SparkPIDcontrols = motor1.getPIDController();
+    private RelativeEncoder m1_encoder = motor1.getEncoder();
+    
+    
     private double maxRPM = 5700;
 
     private double kP; 
@@ -36,6 +42,8 @@ public class MotorTest extends SubsystemBase{
     private double kFF; 
     private double kMaxOutput; 
     private double kMinOutput;
+
+    private double setPoint;
 
     public MotorTest ()
     {
@@ -54,6 +62,9 @@ public class MotorTest extends SubsystemBase{
         SmartDashboard.putNumber("Feed Forward", kFF);
         SmartDashboard.putNumber("Max Output", kMaxOutput);
         SmartDashboard.putNumber("Min Output", kMinOutput);
+        SmartDashboard.putNumber("Set Point", setPoint);
+
+        SmartDashboard.putNumber("Velocity", m1_encoder.getVelocity());
 
         SparkPIDcontrols.setP(kP);
         SparkPIDcontrols.setI(kI);
@@ -87,16 +98,16 @@ public class MotorTest extends SubsystemBase{
 
     public void setDrivePower1(double v)
     {
+        setPoint=v*maxRPM;
         //System.out.println(SparkPIDcontrols.getP());
         if(v>0.1||v<-0.1)
         {
-            //motor1.set(v);
-            double setPoint = v*maxRPM;
             SparkPIDcontrols.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
         }
         else
         {
-            driveMotorStop1();
+            //driveMotorStop1();
+            SparkPIDcontrols.setReference(0, CANSparkMax.ControlType.kVelocity);
         }
     }
 
@@ -155,6 +166,10 @@ public class MotorTest extends SubsystemBase{
 
     private void updateTuning() 
     {
+        SmartDashboard.putNumber("Set Point", setPoint);
+
+        SmartDashboard.putNumber("Velocity", m1_encoder.getVelocity());
+
         if (kP != SmartDashboard.getNumber("P Value", 0))
         {
             kP = SmartDashboard.getNumber("P Value", 0);
@@ -185,6 +200,6 @@ public class MotorTest extends SubsystemBase{
             kMaxOutput = SmartDashboard.getNumber("Max Output", 0);
             kMinOutput = SmartDashboard.getNumber("Min Output", 0);
             SparkPIDcontrols.setOutputRange(kMinOutput, kMaxOutput);
-        }
+        }    
     }
 } 
